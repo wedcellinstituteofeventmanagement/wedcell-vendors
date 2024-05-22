6f7c4c78e80bf8d45a50a7cadaa356ae50ab5149
Abialidr <luckybelt79@gmail.com>
2024-05-22 13:01:31 +0530

    a

--- config/index.js
+++ config/index.js
@@ -1,8 +1,10 @@
 // export const PROXY =
 //   "https://ec2-13-235-133-97.ap-south-1.compute.amazonaws.com";
-export const PROXY = process.env.NEXT_PUBLIC_PROXY;
+// export const PROXY = process.env.NEXT_PUBLIC_PROXY;
+
 // export const PROXY = 'http://localhost:8080';
+export const PROXY = "https://backend.wedcell.com";
 
 export const firebaseUrl =
-  'https://wedcell-743f6-default-rtdb.asia-southeast1.firebasedatabase.app';
+  "https://wedcell-743f6-default-rtdb.asia-southeast1.firebasedatabase.app";
 //
