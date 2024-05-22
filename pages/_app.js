78736a1c2ea7296f624431ab42b9a33da72c96e4
Abialidr <luckybelt79@gmail.com>
2024-05-22 13:20:24 +0530

    added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import "../styles/bootstrap.min.css";
+import "../styles/bootstrap-custom.css";
 import "../styles/index.css";
 import Layout from "../Components/layout/Layout";
 import { Provider } from "react-redux";
